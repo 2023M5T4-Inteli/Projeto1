5225f241e4800f1231ddf74bf6b2a60928b41174
edu-porto <99202389+edu-porto@users.noreply.github.com>
2023-02-24 13:40:20 -0300

    Contrato correto adicionado

--- contrato/etherchain.sol
+++ contrato/etherchain.sol
@@ -1,41 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Este código é para teste e para ser modificados pelos integrantes do Grupo 1
-
 contract ContratoCoover {
     // Variáveis globais
-    uint public saldoContrato;
-    uint public quantUsuario;
-    uint public dataCriacao;
-    uint public dataValidade;
-    uint public minPessoas = 5;
-    uint public maxPessoas = 50;
-    uint public duracaoDias; 
-    address private _admin;
-    
+    uint public id; //
+    uint public amountContract; // 
+    uint public userQuantity; // 
+    uint public creationDate; //  
+    uint public dataValidade; // 
+    uint public minPeople = 5; // 
+    uint public maxPeople = 50; // 
+    uint public plusDays; //
+    address private _admin; 
+    address[] public group;
+    uint256 public totalMembers;
+    address private _owner;
 
-    struct Carteiras {
-        address carteiraUsuario;
+    struct Wallets {
+        address userWallet;
     }
     
-    address[] public carteira;
+    address[] public wallet;
     
     mapping(address => bool) public termoAceito;
-    uint public numAceitaram;
+
+    mapping(address => uint256) private indemnityRequests;
 
     // Construtor
-    constructor(uint _usuarios, uint _usuarioMinimo, uint _usuarioMaximo, uint _duracaoDias) {
+    // Este constructor roda apenas no deploy e tem a função de guarda algumas informações muito importantes
+    constructor(uint _users, uint _usersMin, uint _userMax, uint _plusDays) payable{
         _admin = msg.sender;
-        dataCriacao = block.timestamp; //guarda a data de criação
-        saldoContrato = address(this).balance;
-        quantUsuario = _usuarios;
-        minPessoas = _usuarioMinimo;
-        maxPessoas = _usuarioMaximo;
-        dataValidade = dataCriacao + _duracaoDias * 1 days; // guarda a data de validade
+        creationDate = block.timestamp; //guarda a data de criação
+        amountContract = address(this).balance;
+        userQuantity = _users;
+        minPeople = _usersMin;
+        maxPeople = _userMax;
+        dataValidade = creationDate + _plusDays * 1 days; // guarda a data de validade
     }
 
-    // Funções
+
+    // Modificador que tem a função de checar se o contrato esta ativo ou não 
+    modifier viabilidade(){
+        if (userQuantity >= minPeople && block.timestamp <= dataValidade && userQuantity <= maxPeople) {
+                _; // Contrato Ativo
+            } else if (userQuantity < minPeople && block.timestamp <= dataValidade) {
+                _; // Contrato em Progresso
+            } else if (block.timestamp > dataValidade || userQuantity < minPeople) {
+                revert("Contrato inativo"); // Contrato Inativo
+            } else {
+                revert("Erro ao verificar o contrato");
+        }
+    }
+
+
 
     // Modificador para permitir que apenas o dono do contrato acesse uma função
     modifier onlyOwner() {
@@ -43,96 +60,111 @@ contract ContratoCoover {
         _;
     }
 
-    // modifier isValid(){
-    //     // require(block.timestamp => *variável de data de validade*)
-    //     _;
-    // }
 
-//lembrar de adicionar nas funções devidas
-    modifier viabilidade(){
-        if (quantUsuario >= minPessoas && block.timestamp <= dataValidade && quantUsuario <= maxPessoas) {
-                _; // Contrato Ativo
-            } else if (quantUsuario < minPessoas && block.timestamp <= dataValidade) {
-                _; // Contrato em Progresso
-            } else if (block.timestamp > dataValidade || quantUsuario < minPessoas) {
-                revert("Contrato inativo"); // Contrato Inativo
-            } else {
-                revert("Erro ao verificar o contrato");
+
+    //User story 2 
+    //Função que permite visualizar o contrato na blockchain e poder escolher algum para participar
+    function checkGroups () public {
+    }
+
+    // User story 3
+    // função que permite a solicitação de uma indenização e armazena o endereço do solicitante
+    function requestIndemnity(uint256 amount) public {
+        require(amount > 0, "Indenizacao tem de ser maior que 0");
+        indemnityRequests[msg.sender] = amount;
+    }
+
+
+    // User story 5 
+    // Função que permite ao usuario solicitar o reembolso
+    function redeemnRefund() private{
+
+    }
+
+    // User story 6
+    // Função que permite o cliente ver os termos do contrato 
+    function checkTerms() public {
+
+    }
+
+    // User story 7
+    // Criação de um grupo a partir das Wallets
+    function groupCreation(address[] memory walletsToAdd) onlyOwner public {
+        require(group.length == 0, "O grupo ja foi criado");
+        totalMembers = walletsToAdd.length + 1;
+        group = new address[](totalMembers);
+        group[0] = _owner;
+        for (uint256 i = 0; i < walletsToAdd.length; i++) {
+            group[i + 1] = walletsToAdd[i];
         }
     }
 
-    // function mostrarSaldo() public view returns (uint) {
-    //     return saldoContrato;
-    // }
+    // User story 8
+    // A Coover tem a possibilidade de visualizar o grupo criado por ela e também excluir o mesmo
+    function showDeleteGroup() private onlyOwner{
 
-    function quantCarteira(address novaCarteira) public onlyOwner returns(bool) {
-        require(carteira.length < maxPessoas, "Numero maximo de pessoas na carteiras foi atingido.");
-        // Carteiras memory novaCarteira = Carteiras(msg.sender);
-        carteira.push(novaCarteira);
-        quantUsuario += 1;
+    }
+
+
+    // User story 9
+    /* Permite que o proprietário aceite uma solicitação de indenização
+    ao final, se todas as condições forem atendidas, o valor solicitado é
+    transferido para o endereço do solicitante e a solicitação é excluída. */
+    
+    function acceptIndemnityRequest(address requestor) public onlyOwner {
+        require(msg.sender == _owner,"Somente o dono pode aceitar reembolsos ");
+        uint256 amountToPay = indemnityRequests[requestor];
+        require(amountToPay > 0, "Pedido de reembolso nao encontrado");
+        delete indemnityRequests[requestor];
+        payable(requestor).transfer(amountToPay);
+    }
+
+    // User story 10 
+    // O administrador consegue editar as regras do contrato 
+    function changeRules() public{
+
+    }
+   
+
+    // A função abaixo aparece para todos os visualizadores da Blockchain, porem, somente o dono consegue editar a função
+    // Além disso, essa função delimita o numero de pessoas que podem estar dentro da carteira  
+
+    function quantWallet(address newWallet) public onlyOwner returns(bool) {
+        require(wallet.length < maxPeople, "Numero maximo de pessoas na Wallets foi atingido.");
+        wallet.push(newWallet);
+        userQuantity += 1;
         return true;
     }
 
-    // function quantUsuarios() public view returns (uint) {
-    //     return quantUsuario;
-    // }
-
-    // function dataInicialCriacao() public view returns (uint) {
-    //     return dataCriacao;
-    // }
     
     // Adiciona um novo usuário ao projeto
 
-    function adicionarUsuario() public onlyOwner returns(bool) {
-        require(quantUsuario < maxPessoas, "O numero maximo de usuarios ja foi atingido.");
-        quantUsuario++;
+    function addUser() public onlyOwner returns(bool) {
+        require(userQuantity < maxPeople, "O numero maximo de usuarios ja foi atingido.");
+        userQuantity++;
         return true;
     }
 
 
 
-    function removerUsuario(address usuario) public onlyOwner returns(bool){
+    // Remove um usuário do projeto
 
-        for (uint i = 0; i < carteira.length - 1; i++) {
-            if (carteira[i] == usuario) {
+    function removeUser(address user) public onlyOwner returns(bool){
+
+        for (uint i = 0; i < wallet.length - 1; i++) {
+            if (wallet[i] == user) {
                 
-                for(uint index = i; i < carteira.length - 1; index++){
-                    carteira[index] = carteira[index + 1];
+                for(uint index = i; i < wallet.length - 1; index++){
+                    wallet[index] = wallet[index + 1];
                 }
                 
             }
 
         }   
 
-        carteira.pop();
+        wallet.pop();
         return true;
     }
 
 
-    // function renovarContrato(uint _novaDataValidade) public onlyOwner {
-    //     require(_novaDataValidade > block.timestamp, "A nova data de validade deve ser no futuro.");
-
-    //     // Cria um array para armazenar os índices dos usuários que não aceitaram o novo termo
-    //     uint[] memory indicesRemover = new uint[](quantUsuario);
-    //     uint quantRemover = 0;
-
-    //     // Verifica se cada usuário aceitou o termo, adicionando o índice à lista de remoção, caso contrário
-    //     for (uint i = 0; i < quantUsuario; i++) {
-    //         if (!termoAceito[usuario[i]]) {
-    //             indicesRemover[quantRemover] = i;
-    //             quantRemover++;
-    //         }
-    //     }
-
-    //     // Remove os usuários que não aceitaram o novo termo
-    //     for (uint i = 0; i < quantRemover; i++) {
-    //         removerUsuario(usuario[indicesRemover[i]]);
-    //     }
-
-    //     // Verifica se a quantidade de usuários é compatível com o mínimo e o máximo definidos no contrato
-    //     require(quantUsuario >= minPessoas && quantUsuario <= maxPessoas, "A quantidade de usuarios nao e compativel com o minimo e o maximo definidos no contrato.");
-        
-    //     dataValidade = _novaDataValidade;
-
-    // }
 }
