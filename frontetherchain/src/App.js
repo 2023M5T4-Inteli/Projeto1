2f6bba343b4362a04ec842b34c05631ea4f6e501
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2023-03-15 15:54:59 -0300

    Conect Wallet

--- frontetherchain/src/App.js
+++ frontetherchain/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import MetamaskPlugin from './pages/MetamaskPlugin';
 
 function App() {
--- frontetherchain/src/pages/MetamaskPlugin.jsx
+++ frontetherchain/src/pages/MetamaskPlugin.jsx
@@ -1,33 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Web3 from "web3";
+import { Grid, Button, Typography } from '@mui/material';
 
 function MetamaskPlugin() {
   const [web3, setWeb3] = useState(null);
   const [account, setAccount] = useState(null);
+  
+  let accountMessage;
+  if (account === '0xa930575a2cca74cc4db44edd260a4eb489709a79') {
+    accountMessage = 'Coover';
+  } else {
+    accountMessage = account;
+  }
 
-  useEffect(() => {
-    // Detecta se há uma instância da Metamask na janela do navegador
-    if (window.ethereum) {
-      // Cria uma nova instância do Web3 usando o provedor da Metamask
-      const web3 = new Web3(window.ethereum);
-      setWeb3(web3);
+  const handleConnect = async () => {
+    try {
+      // Detecta se há uma instância da Metamask na janela do navegador
+      if (window.ethereum) {
+        // Cria uma nova instância do Web3 usando o provedor da Metamask
+        const web3 = new Web3(window.ethereum);
+        setWeb3(web3);
 
-      // Solicita ao usuário que autorize o acesso à conta
-      window.ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
+        // Solicita ao usuário que autorize o acesso à conta
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
         // Define a conta atual
         setAccount(accounts[0]);
-      });
-    } else {
-      console.log("Metamask não detectado");
+      } else {
+        console.log("Metamask não detectado");
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }, []);
+  };
 
   return (
     <div>
       {account ? (
-        <p>Conectado à conta {account}</p>
+        <Typography variant="body1">
+          Conectado a conta {accountMessage}
+        </Typography>
       ) : (
-        <p>Não conectado à conta da Metamask</p>
+        <div>
+          <Grid sx={{display:'flex', justifyContent:'center', paddingTop:45}}>
+          <Button variant="contained" onClick={handleConnect} size='large' sx={{borderRadius:'15px', backgroundColor:'#EAEAEA', color:'black'}}>
+            Conecte sua carteira
+          </Button>
+          </Grid>
+          <br/><br/>
+          <hr/>
+          <Grid sx={{display:'flex', justifyContent:'center', paddingTop:5,}}>
+          <Button sx={{borderRadius:'15px', backgroundColor:'#02DE82'}} variant="contained" size='large' >
+            Crie sua MetaMask
+          </Button>
+          </Grid>
+        </div>
       )}
     </div>
   );
