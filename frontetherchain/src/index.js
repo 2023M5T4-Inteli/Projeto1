20a61c957134ba56dcaa8a18c263d8b5ba03e9c3
Vfernandes1 <vinicius.fernandes@sou.inteli.edu.br>
2023-03-15 17:51:13 -0300

    update routes
    atualização das routes para visualizar outras páginas

--- frontetherchain/src/index.js
+++ frontetherchain/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import visual from './pages/visualizar_Grupos'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Meta from './pages/MetamaskPlugin'
+
+
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <Meta/>
+  },
+  {
+    path: "/visual",
+    element: <visual/>,
+  }
+
+
+
+])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
